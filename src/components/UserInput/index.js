2995038312b3ee97a158d88e9a751820771b5692
Gabriel Bastos <ggbstos@gmail.com>
2022-06-29 16:25:09 -0300

    Full app restructuration: single pokemon search; functional user inputs, the pokemon image is now displayed on the screen; sucessful API requests!

--- src/components/UserInput/index.js
+++ src/components/UserInput/index.js
@@ -1,46 +1,52 @@
 import './style.css';
 import axios from 'axios';
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button, Image } from '@chakra-ui/react'
 import { Input } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
+import { useState } from 'react'
 
 export default function UserInput() {
   const BASEURL = 'https://pokeapi.co/api/v2/pokemon/';
-  const POKEMONS = ['ditto', 'charmander', 'bulbasaur', 'gardevoir', 'abra'];
 
-  let pokemonData = [];
-    // Esse array foi criado com o propósito de armazenar os dados extraídos da
-    // API, para que seja possível acessá-los no escopo global do código. Ou seja,
-    // é possível acessar os dados solicitados de fora da função que cumpre esse
-    // papel.
-  //FIXME: Estudar async, promises e closures para solicitar os dados da API
-  POKEMONS.forEach((POKEMON) => { 
-    const URL = BASEURL + POKEMON;
+  // TODO: Estudar useState()
+  const [pokemonData, setPokemonData] = useState()
+
+  function getInputValue (e) {
+    const POKEMON = e.target[0].value
+    const URL = BASEURL + POKEMON.toLowerCase();
+    
+    // FIXME: Modificar error case - substituir o window.alert por um pop-up na página
+    POKEMON ? console.log(POKEMON) : window.alert("Insira algum pokemon")
+
     axios.get(URL).then((res) => {
-      // console.log(res.data);
-        // Acessando dados de cada pokemon
-      pokemonData.push(res.data);
-      console.log(pokemonData); 
-      // Terceiro retorno no console:
-      // [{...}]; [{...}, {...}]; [{...}, {...}, {...}]; [{...}, {...}, {...}, 
-      // {...}] ... [{...} * 5]
-    })
-    console.log(pokemonData); // [] * 5
-      // Primeiro retorno no console
-  })
+      setPokemonData(res.data)
+      console.log(pokemonData);
+  }).catch(() => {
+      // FIXME: Modificar error case - substituir o window.alert por um pop-up na página
+      window.alert("Este pokemon não existe :(");
+  });
 
-  console.log(pokemonData); // [] * 2
-    // Segundo retorno no console
+    e.preventDefault();
+  }
 
   return(
     <div>
       <main id="titleAndInput">
         <img src="/assets/images/Pokeinfo.png" alt="PokeInfo"/>
 
-        <div id="inputAndButton">
+        <form id="inputAndButton" onSubmit={getInputValue}>
+          {/* FIXME: Alterar design para melhorar o contraste */}
           <Input id="userInput" type="text" placeholder="Nome do Pokemon"/>
-          <Button type="button" id="search" variant="outline">Catch!</Button>
-        </div>
-      
+          <Button id="search" type="submit" variant="outline">Catch!</Button>
+        </form>
+        {/*TODO: Transformar o card num componente para tornar o código mais readable?
+          *TODO: Incluir demais atributos do pokemon no card*/}
+        {pokemonData ? 
+          <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' mt='10'>
+            <Image src={pokemonData.sprites.front_default}/>
+          </Box> 
+        : <></>}
+        
       </main>
     </div>
   ) 
