d8ead272cb16545348f45bcdda7b0fb00a4b1198
Gabriel Bastos <ggbstos@gmail.com>
2022-07-04 20:35:36 -0300

    Working on error cases: trying to show an alert using ChakraUI Alert and useState()

--- src/components/UserInput/index.js
+++ src/components/UserInput/index.js
@@ -1,32 +1,40 @@
 import './style.css';
 import axios from 'axios';
+import { useState } from 'react'
 import { Button, Image } from '@chakra-ui/react'
 import { Input } from '@chakra-ui/react'
 import { Box } from '@chakra-ui/react'
-import { useState } from 'react'
+import { Alert, AlertIcon } from '@chakra-ui/react'
 
 export default function UserInput() {
   const BASEURL = 'https://pokeapi.co/api/v2/pokemon/';
 
   // TODO: Estudar useState()
   const [pokemonData, setPokemonData] = useState()
+  const [showAlert, setShowAlert] = useState(false)
 
-  function getInputValue (e) {
-    const POKEMON = e.target[0].value
+  function getInputValue (pokemonName) {
+    const POKEMON = pokemonName.target[0].value
     const URL = BASEURL + POKEMON.toLowerCase();
+    console.log(pokemonName)
     
     // FIXME: Modificar error case - substituir o window.alert por um pop-up na página
-    POKEMON ? console.log(POKEMON) : window.alert("Insira algum pokemon")
+    if (!POKEMON) {
+      window.alert("Insira o nome de algum pokemon!")
+      return pokemonName.preventDefault()
+    }
 
     axios.get(URL).then((res) => {
       setPokemonData(res.data)
       console.log(pokemonData);
   }).catch(() => {
       // FIXME: Modificar error case - substituir o window.alert por um pop-up na página
-      window.alert("Este pokemon não existe :(");
+      // TODO: Devo continuar a utilizar o useState() para esse propósito?
+      // setShowAlert(true)
+      window.alert("Este pokemon não existe :(")
   });
 
-    e.preventDefault();
+    pokemonName.preventDefault();
   }
 
   return(
@@ -39,13 +47,30 @@ export default function UserInput() {
           <Input id="userInput" type="text" placeholder="Nome do Pokemon"/>
           <Button id="search" type="submit" variant="outline">Catch!</Button>
         </form>
+
         {/*TODO: Transformar o card num componente para tornar o código mais readable?
           *TODO: Incluir demais atributos do pokemon no card*/}
+
         {pokemonData ? 
           <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' mt='10'>
             <Image src={pokemonData.sprites.front_default}/>
           </Box> 
         : <></>}
+
+        {/* Possível solução encontrada no stackoverflow: utilizar useState() para
+        lidar com o error case de pokemon inexistente.
+        fonte: https://stackoverflow.com/questions/72209574/how-to-display-the-chakra-ui-alert#comment127580507_72209574 */}
+
+        {/* <>
+        {showAlert && (
+          <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' mt='20'>
+            <Alert status="warning">
+              <AlertIcon/>
+              Este pokemon não existe :(
+            </Alert>
+          </Box>
+        )}
+        </> */}
         
       </main>
     </div>
