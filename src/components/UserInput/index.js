9e3ae589d6497da8ed16f94de4d6f584136cab0b
Gabriel Bastos <ggbstos@gmail.com>
2022-08-31 21:43:58 -0300

    Some refactoring and styling

--- src/components/UserInput/index.js
+++ src/components/UserInput/index.js
@@ -4,20 +4,20 @@ import { useState } from 'react'
 import { Button, Image } from '@chakra-ui/react'
 import { Input } from '@chakra-ui/react'
 import { Box } from '@chakra-ui/react'
-import { type } from '@testing-library/user-event/dist/type';
 // import { Alert, AlertIcon } from '@chakra-ui/react'
 
 export default function UserInput() {
+  // API Endpoint
   const BASEURL = 'https://pokeapi.co/api/v2/pokemon/';
 
-  // TODO: Estudar useState()
+  // TODO: Estudar mais sobre useState()
   const [pokemonData, setPokemonData] = useState();
   // const [showAlert, setShowAlert] = useState(false);
 
-  function getInputValue (pokemonName) {
+  function getUserInputValue (pokemonName) {
     const POKEMON = pokemonName.target[0].value;
     const URL = BASEURL + POKEMON.toLowerCase();
-    console.log(pokemonName);
+    console.log(pokemonName); // Retirar ao finalizar projeto
     
     // FIXME: Modificar error case - substituir o window.alert por um pop-up na página
     if (!POKEMON) {
@@ -25,45 +25,41 @@ export default function UserInput() {
       return pokemonName.preventDefault();
     };
 
-    axios.get(URL).then((res) => {
-      setPokemonData(res.data);
-      console.log(pokemonData);
-  }).catch(() => {
+    axios.get(URL).then((response) => {
+      setPokemonData(response.data);
+      console.log(pokemonData); // Retirar ao finalizar projeto
+    }).catch(() => {
       // FIXME: Modificar error case - substituir o window.alert por um pop-up na página
         // FIXME: Devo continuar a utilizar o useState() para esse propósito?
       // setShowAlert(true)
       window.alert("Este não é um pokémon :(");
-  });
+    });
 
     pokemonName.preventDefault();
   };
 
   return(
-    <div>
       <main id="titleAndInput">
         <img src="/assets/images/Pokeinfo.png" alt="PokeInfo"/>
 
-        <form id="inputAndButton" onSubmit={getInputValue}>
+        <form id="inputAndButton" onSubmit={getUserInputValue}>
           {/* FIXME: Alterar design para melhorar o contraste */}
-          <Input id="userInput" type="text" placeholder="Nome do Pokemon"/>
+          <Input id="userInput" type="text" placeholder="Nome do Pokémon"/>
           <Button id="search" type="submit" variant="outline">Catch!</Button>
         </form>
 
-        {/*TODO: Transformar o card num componente para tornar o código mais readable?
-          *TODO: Incluir demais atributos do pokemon no card*/}
+        {/*TODO: Transformar o card num componente para tornar o código mais readable*/}
 
         {pokemonData ? 
           <Box id="card">
-            <p id="pokemonName">{pokemonData.name}
-              <span className="pokemonId">#{pokemonData.id}</span>
-            </p>
-            <p id="pokemonType">{pokemonData.types.map(typeObject => typeObject.type.name).join(' ')}</p>
-            <Image src={pokemonData.sprites.front_default}/>
+            <p id="pokemonName">{pokemonData.name}</p>
+            <p id="pokemonId">#{pokemonData.id}</p>
+            <p id="pokemonType">{pokemonData.types.map(types => types.type.name).join(' ')}</p>
+            <Image id ="pokemonImage" src={pokemonData.sprites.front_default}/>
           </Box> 
         : <></>}
 
-        {/* Possível solução encontrada no stackoverflow: utilizar useState() para
-        lidar com o error case de pokemon inexistente.
+        {/* Possível solução: utilizar useState() para lidar com o error case de pokemon inexistente.
         fonte: https://stackoverflow.com/questions/72209574/how-to-display-the-chakra-ui-alert#comment127580507_72209574 */}
 
         {/* <>
@@ -78,6 +74,5 @@ export default function UserInput() {
         </> */}
         
       </main>
-    </div>
   ); 
 };
