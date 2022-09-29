7af84a304860926d912b3987f3ce01213198e7d9
Gabriel Bastos <ggbstos@gmail.com>
2022-09-29 00:47:59 -0300

    Main component renamed, footer component included, cleaned up default css parameters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import UserInputAndCard from './components/UserInputAndCard';
+import UserInputAndCard from './components/UserInputAndCard/userInputAndCard.js';
+import Footer from './components/Footer/footer.js'
 
 export default function App() {
   return (
     <div className="App">
       <UserInputAndCard />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css';
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Dados extraídos da <a href="https://pokeapi.co/" target="_blank" rel="noreferrer">PokeAPI</a>
+      </p>
+      <p>Feito com ❤ por Gabriel Bastos!</p>
+    </footer>
+  )
+}
